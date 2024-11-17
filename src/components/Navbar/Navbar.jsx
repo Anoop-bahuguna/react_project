e7230a6a9ab337dd34b76c193a122c696c74e4d8
Anoop <anup56.ab@gmail.com>
2024-11-17 12:29:47 +0530

    change in css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">
-        Portflio
+        Portfolio
       </a>
       <div className={styles.menu}>
         <img
